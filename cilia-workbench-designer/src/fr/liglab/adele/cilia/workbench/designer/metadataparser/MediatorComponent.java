cf31b7c2fba8a1ce53bf622389c8547b2b0317b3
Etienne Gandrille <etienne.gandrille@imag.fr>
2011-09-27 12:06:42 +0200

    First properties view implem form Repository viewer

--- cilia-workbench-designer/src/fr/liglab/adele/cilia/workbench/designer/metadataparser/MediatorComponent.java
+++ cilia-workbench-designer/src/fr/liglab/adele/cilia/workbench/designer/metadataparser/MediatorComponent.java
@@ -7,9 +7,9 @@ public class MediatorComponent {
 	private String name;
 	private String category;
 	
-	private String schedulerName;
-	private String processorName;
-	private String dispatcherName;
+	private String internal_schedulerName;
+	private String internal_processorName;
+	private String internal_dispatcherName;
 	
 	
 	public MediatorComponent(Node node) throws MetadataException {
@@ -20,17 +20,17 @@ public class MediatorComponent {
 		Node schedulerNode = XMLutil.findChild(node, "scheduler");
 		if (schedulerNode == null)
 			throw new MetadataException("scheduler element not found");
-		XMLutil.setAttribute(schedulerNode, "name", this, "schedulerName");
+		XMLutil.setAttribute(schedulerNode, "name", this, "internal_schedulerName");
 		
 		Node processorNode = XMLutil.findChild(node, "processor");
 		if (processorNode == null)
 			throw new MetadataException("processor element not found");
-		XMLutil.setAttribute(processorNode, "name", this, "processorName");
+		XMLutil.setAttribute(processorNode, "name", this, "internal_processorName");
 		
 		Node dispatcherNode = XMLutil.findChild(node, "dispatcher");
 		if (dispatcherNode == null)
 			throw new MetadataException("dispatcher element not found");
-		XMLutil.setAttribute(dispatcherNode, "name", this, "dispatcherName");
+		XMLutil.setAttribute(dispatcherNode, "name", this, "internal_dispatcherName");
 	}
 	
 	@Override
