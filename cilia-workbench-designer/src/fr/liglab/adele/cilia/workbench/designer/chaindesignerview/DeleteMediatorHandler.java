1d80be3fcc2573fbffd78069f4eb3887cf10e562
Etienne Gandrille <etienne.gandrille@imag.fr>
2011-09-30 18:25:00 +0200

    Toolbar implem (only skeleton) for Chain Designer

---
+++ cilia-workbench-designer/src/fr/liglab/adele/cilia/workbench/designer/chaindesignerview/DeleteMediatorHandler.java
@@ -0,0 +1,17 @@
+package fr.liglab.adele.cilia.workbench.designer.chaindesignerview;
+
+import org.eclipse.core.commands.ExecutionEvent;
+import org.eclipse.core.commands.ExecutionException;
+import org.eclipse.jface.dialogs.MessageDialog;
+
+import fr.liglab.adele.cilia.workbench.designer.dsciliarepositoryview.CommonHandler;
+
+public class DeleteMediatorHandler extends CommonHandler {
+
+	@Override
+	public Object execute(ExecutionEvent event) throws ExecutionException {
+		MessageDialog.openInformation(getShell(event), "Handler", this.getClass().getName());
+		return null;
+	}
+
+}
