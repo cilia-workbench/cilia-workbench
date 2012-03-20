801ed03762d8bb9b0ff8d0ea2bf80e8cc73ad1e0
Etienne Gandrille <etienne.gandrille@gmail.com>
2012-03-20 18:38:38 +0100

    License checker update (was duplicated). Please only use cilia-workbench-helpers/license-checker from now.

--- cilia-jface/src/fr/liglab/adele/cilia/dialog/editor/ListEditor.java
+++ cilia-jface/src/fr/liglab/adele/cilia/dialog/editor/ListEditor.java
@@ -1,10 +1,10 @@
-/*
- * Copyright Adele Team LIG (http://www-adele.imag.fr/)
+/**
+ * Copyright Universite Joseph Fourier (www.ujf-grenoble.fr)
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *   http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -34,6 +34,10 @@ import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Shell;
 import org.eclipse.swt.widgets.Text;
 
+/**
+ * 
+ * @author Etienne Gandrille
+ */
 public class ListEditor extends AbstractEditor {
 
 	private List<String> input;
