f4e841398a6adf394bd1aee97c4fd4358225e200
Etienne Gandrille <etienne.gandrille@imag.fr>
2012-03-26 17:42:58 +0200

    JFace Sandbox : new mediator dialog.

---
+++ cilia-jface/src/fr/liglab/adele/cilia/dialog/newMediatorDialog/NewMediatorDialog.java
@@ -0,0 +1,166 @@
+/**
+ * Copyright Universite Joseph Fourier (www.ujf-grenoble.fr)
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package fr.liglab.adele.cilia.dialog.newMediatorDialog;
+
+import org.eclipse.jface.dialogs.Dialog;
+import org.eclipse.jface.dialogs.IDialogConstants;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.Event;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Listener;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Text;
+
+import fr.liglab.adele.cilia.dialog.editor.Utilities;
+
+/**
+ * 
+ * @author Etienne Gandrille
+ */
+public class NewMediatorDialog extends Dialog {
+
+	private final String title = "New mediator specification";
+
+	private final String introMessage = "Please give an id and a name for the new mediator specification.";
+
+	private final String idLabelText = "id";
+
+	private final String namespaceLabelText = "namespace";
+
+	private Text idText = null;
+
+	private Text namespaceText = null;
+
+	private Label errorLabel = null;
+
+	private String id;
+
+	private String namespace;
+
+	public NewMediatorDialog(Shell parent) {
+		super(parent);
+	}
+
+	protected Control createDialogArea(Composite parent) {
+		Composite container = (Composite) super.createDialogArea(parent);
+
+		getShell().setText(title);
+
+		// Global layout
+		GridLayout mainLayout = new GridLayout(2, false);
+		container.setLayout(mainLayout);
+
+		// intro
+		final Label introLabel = new Label(container, SWT.WRAP);
+		introLabel.setText(introMessage);
+		introLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false, 2, 1));
+
+		// id
+		final Label idLabel = new Label(container, SWT.WRAP);
+		idLabel.setText(idLabelText);
+		idLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false));
+
+		idText = new Text(container, SWT.WRAP);
+		idText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		idText.addListener(SWT.Verify, Utilities.getTextValidatorListner());
+		idText.addListener(SWT.Modify, new Listener() {
+			@Override
+			public void handleEvent(Event event) {
+				onTextUpdate();
+			}
+		});
+
+		// namespace
+		final Label namespaceLabel = new Label(container, SWT.WRAP);
+		namespaceLabel.setText(namespaceLabelText);
+		namespaceLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, false, false));
+
+		namespaceText = new Text(container, SWT.WRAP);
+		namespaceText.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, false));
+		namespaceText.addListener(SWT.Verify, Utilities.getTextValidatorListner());
+		namespaceText.addListener(SWT.Modify, new Listener() {
+			@Override
+			public void handleEvent(Event event) {
+				onTextUpdate();
+			}
+		});
+
+		// error message area
+		errorLabel = new Label(container, SWT.WRAP);
+		errorLabel.setText("");
+		errorLabel.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true, 2, 1));
+
+		return container;
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.dialogs.Dialog#initializeBounds()
+	 */
+	@Override
+	protected void initializeBounds() {
+		super.initializeBounds();
+		getButton(IDialogConstants.OK_ID).setEnabled(false);
+
+	}
+
+	private void onTextUpdate() {
+		id = idText.getText();
+		namespace = namespaceText.getText();
+
+		if (id.isEmpty() || namespace.isEmpty()) {
+			errorLabel.setText("ça peut pas être null !");
+			getButton(IDialogConstants.OK_ID).setEnabled(false);
+
+		} else {
+			errorLabel.setText("");
+			getButton(IDialogConstants.OK_ID).setEnabled(true);
+		}
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.dialogs.Dialog#createButtonsForButtonBar(org.eclipse .swt.widgets.Composite)
+	 */
+	protected void createButtonsForButtonBar(Composite parent) {
+		createButton(parent, IDialogConstants.OK_ID, IDialogConstants.OK_LABEL, true);
+		createButton(parent, IDialogConstants.CANCEL_ID, IDialogConstants.CANCEL_LABEL, false);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.dialogs.Dialog#getInitialSize()
+	 */
+	protected Point getInitialSize() {
+		return new Point(450, 200);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see org.eclipse.jface.dialogs.Dialog#isResizable()
+	 */
+	protected boolean isResizable() {
+		return true;
+	}
+}
