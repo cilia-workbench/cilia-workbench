aa048a45a28f92ce7c344232852f5f2c24d4f243
Etienne Gandrille <etienne.gandrille@gmail.com>
2012-06-22 09:49:29 +0200

    DSCilia Repository View Toolbar skeleton

---
+++ cilia-workbench-designer/src/fr/liglab/adele/cilia/workbench/designer/view/repositoryview/dsciliaview/DSCiliaViewHandler.java
@@ -0,0 +1,9 @@
+package fr.liglab.adele.cilia.workbench.designer.view.repositoryview.dsciliaview;
+
+import fr.liglab.adele.cilia.workbench.designer.view.repositoryview.common.RepositoryViewHandler;
+
+public abstract class DSCiliaViewHandler extends RepositoryViewHandler {
+	public DSCiliaViewHandler() {
+		super(DSCiliaRepositoryView.VIEW_ID);
+	}
+}
