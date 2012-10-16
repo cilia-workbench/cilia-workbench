a69563fc6a1b1c1681f97e2f6c5ec8330ce8e33d
Etienne Gandrille <etienne.gandrille@gmail.com>
2012-10-16 17:10:26 +0200

    Toolbar implementation in progress... handlers Skeleton

--- cilia-workbench-rest-monitoring/src/fr/liglab/adele/cilia/workbench/restmonitoring/view/platformview/DeleteFileHandler.java
+++ cilia-workbench-rest-monitoring/src/fr/liglab/adele/cilia/workbench/restmonitoring/view/platformview/DeleteFileHandler.java
@@ -2,45 +2,12 @@ package fr.liglab.adele.cilia.workbench.restmonitoring.view.platformview;
 
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.commands.IHandler;
-import org.eclipse.core.commands.IHandlerListener;
 
-public class DeleteFileHandler implements IHandler {
-
-	@Override
-	public void addHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void dispose() {
-		// TODO Auto-generated method stub
-
-	}
+public class DeleteFileHandler extends PlatformViewHandler {
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		// TODO Auto-generated method stub
 		return null;
 	}
-
-	@Override
-	public boolean isEnabled() {
-		// TODO Auto-generated method stub
-		return true;
-	}
-
-	@Override
-	public boolean isHandled() {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void removeHandlerListener(IHandlerListener handlerListener) {
-		// TODO Auto-generated method stub
-
-	}
-
 }
---
+++ cilia-workbench-rest-monitoring/src/fr/liglab/adele/cilia/workbench/restmonitoring/view/platformview/PlatformViewHandler.java
@@ -0,0 +1,10 @@
+package fr.liglab.adele.cilia.workbench.restmonitoring.view.platformview;
+
+import fr.liglab.adele.cilia.workbench.common.ui.view.repositoryview.RepositoryViewHandler;
+
+public abstract class PlatformViewHandler extends RepositoryViewHandler {
+
+	public PlatformViewHandler() {
+		super(PlatformView.VIEW_ID);
+	}
+}
