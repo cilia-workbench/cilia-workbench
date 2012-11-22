3482979728701b35d3970c3320241c42c6aad1c9
Etienne Gandrille <etienne.gandrille@imag.fr>
2012-11-22 15:59:04 +0100

    safer toString()

--- cilia-workbench-designer/src/fr/liglab/adele/cilia/workbench/designer/parser/element/implem/AdapterSubElementImplem.java
+++ cilia-workbench-designer/src/fr/liglab/adele/cilia/workbench/designer/parser/element/implem/AdapterSubElementImplem.java
@@ -26,6 +26,7 @@ import fr.liglab.adele.cilia.workbench.common.marker.CiliaFlag;
 import fr.liglab.adele.cilia.workbench.common.marker.ErrorsAndWarningsFinder;
 import fr.liglab.adele.cilia.workbench.common.marker.IdentifiableUtils;
 import fr.liglab.adele.cilia.workbench.common.misc.ReflectionUtil;
+import fr.liglab.adele.cilia.workbench.common.misc.Strings;
 import fr.liglab.adele.cilia.workbench.common.parser.element.ParameterDefinition;
 import fr.liglab.adele.cilia.workbench.common.ui.view.propertiesview.DisplayedInPropertiesView;
 
@@ -62,7 +63,7 @@ public abstract class AdapterSubElementImplem implements DisplayedInPropertiesVi
 
 	@Override
 	public String toString() {
-		return name;
+		return Strings.nullToEmpty(name);
 	}
 
 	@Override
