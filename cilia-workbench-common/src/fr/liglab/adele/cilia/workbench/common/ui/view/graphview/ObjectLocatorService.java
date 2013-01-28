a7625d90e6ea4029bdb7800066fb48648cf1b8e5
Etienne Gandrille <etienne.gandrille@gmail.com>
2013-01-28 11:18:33 +0100

    update for Medical Demo

--- cilia-workbench-common/src/fr/liglab/adele/cilia/workbench/common/ui/view/graphview/ObjectLocatorService.java
+++ cilia-workbench-common/src/fr/liglab/adele/cilia/workbench/common/ui/view/graphview/ObjectLocatorService.java
@@ -19,6 +19,8 @@ import java.util.Map;
 
 import org.eclipse.draw2d.geometry.Point;
 
+import fr.liglab.adele.cilia.workbench.common.parser.chain.ComponentRef;
+
 /**
  * 
  * @author Etienne Gandrille
@@ -39,7 +41,58 @@ public class ObjectLocatorService {
 		location.put(object, point);
 	}
 
-	public Point getLocation(Object object) {
+	private Point getLocationByMemory(Object object) {
 		return location.get(object);
 	}
+
+	// TODO For demo purpose... to be removed later
+	private Point getLocationByName(Object object) {
+		String name = null;
+
+		// Find name
+		// =========
+
+		if (object instanceof ComponentRef) {
+			ComponentRef component = (ComponentRef) object;
+			name = component.getId();
+		}
+
+		if (name == null)
+			return null;
+
+		// Lookup by name
+		// ==============
+
+		if (name.equals("hl7-rest-adapter"))
+			return new Point(22, 15);
+		if (name.equals("hl7-filter") || name.equals("hapifilter"))
+			return new Point(188, 15);
+		if (name.equals("continua-filter"))
+			return new Point(306, 15);
+		if (name.equals("phr-translation") || name.equals("translation"))
+			return new Point(476, 15);
+		if (name.equals("phr-integration") || name.equals("integration"))
+			return new Point(687, 15);
+
+		if (name.equals("error-manager"))
+			return new Point(377, 110);
+		if (name.equals("acknowledgement"))
+			return new Point(369, 173);
+
+		return null;
+	}
+
+	public Point getLocation(Object object) {
+
+		Point locByMem = getLocationByMemory(object);
+		if (locByMem != null)
+			return locByMem;
+
+		Point locByName = getLocationByName(object);
+		if (locByName != null)
+			return locByName;
+
+		return null;
+	}
+
 }
